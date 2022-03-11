dd4606807cc71c579b32cc873a0a8d2b0f3320e3
rerecum <kacperkacperkacper132@gmail.com>
2022-03-11 14:47:28 +0100

    commit 2

--- app/src/main/java/com/example/seekbar/MainActivity.kt
+++ app/src/main/java/com/example/seekbar/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.seekbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<SeekBar>(R.id.seekbar).setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                findViewById<ImageView>(R.id.image).rotation = progress.toFloat()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
     }
 }
